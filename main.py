c95c44a8fcd9adc3d3a8e3616bac7e6334445b2d
dexterdev8 <ioan.oara@protonmail.com>
2022-03-17 22:17:09 +0100

    fetch balance from contract

--- main.py
+++ main.py
@@ -3,27 +3,39 @@ import requests
 import os
 import pandas as pd
 from terra_sdk.client.lcd import LCDClient
-from terra_sdk.client.lcd.api import bank
 
 TERRA_ADDRESS = os.environ["TERRA_ADDRESS"]
 PLUNA_CONTRACT = "terra1tlgelulz9pdkhls6uglfn5lmxarx7f2gxtdzh2"
+YLUNA_CONTRACT = "terra17wkadg0tah554r35x6wvff0y5s7ve8npcjfuhz"
 INCREMENT = 0.0001
 
-yLuna = 0
-pLuna = 58241.935
-n_pLuna = pLuna
+
+class Contract:
+    def __init__(self, client, contract_address, address):
+        self.client = client
+        self.contract_address = contract_address
+        self.address = address
+
+    def _query_balance(self):
+        query = {"balance": {"address": self.address}}
+        return self.client.wasm.contract_query(self.contract_address, query)
+
+    def get_balance(self):
+        balance_response = self._query_balance()
+        info_response = self.client.wasm.contract_info(self.contract_address)
+
+        decimals = info_response["init_msg"]["decimals"]
+        return int(balance_response["balance"]) * 10**-decimals
 
 
-def get_balance():
-    terra_client = LCDClient(chain_id="columbus-5", url="https://lcd.terra.dev")
-    print(
-        terra_client.wasm.contract_query(
-            PLUNA_CONTRACT, {"balance": {"address": TERRA_ADDRESS}}
-        )
-    )
-    bank_instance = bank.BankAPI(terra_client)
-    balance = bank_instance.balance(TERRA_ADDRESS)
-    print(balance[0])
+terra_client = LCDClient(chain_id="columbus-5", url="https://lcd.terra.dev")
+
+
+def get_balances():
+    contract_pLuna = Contract(terra_client, PLUNA_CONTRACT, TERRA_ADDRESS)
+    contract_yLuna = Contract(terra_client, YLUNA_CONTRACT, TERRA_ADDRESS)
+
+    return contract_pLuna.get_balance(), contract_yLuna.get_balance()
 
 
 def get_prices():
@@ -35,7 +47,8 @@ def get_prices():
         "symbol"
     )
 
-    luna_price = df.loc["LUNA", "price"]
+    # luna_price = df.loc["LUNA", "price"]
+    # cLuna_price = df.loc["cLUNA", "price"]
     pLuna_price = df.loc["pLUNA", "price"]
     yLuna_price = df.loc["yLUNA", "price"]
 
@@ -44,11 +57,13 @@ def get_prices():
 
 p_pLuna, p_yLuna = get_prices()
 
-get_balance()
+pLuna, yLuna = get_balances()
+n_pLuna = pLuna
+# print(p_pLuna, p_yLuna)
 
 while True:
 
-    if yLuna == approx(pLuna, rel=1e-5):
+    if yLuna == approx(pLuna, rel=1e-4):
         break
     else:
         yLuna += (INCREMENT * p_pLuna) / p_yLuna
